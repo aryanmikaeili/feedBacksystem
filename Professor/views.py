635d4e6088d0d0341db08e3e5636fa56678ba78d
ce441-981-95105379 <alireza_akbari34@yahoo.com>
2019-12-10 00:34:25 +0330

    Khoobim

--- Professor/views.py
+++ Professor/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from .models import Professor
+from django.contrib.auth.decorators import login_required
+# Create your views here.
+@login_required
 def professor_view(request, id):
     obj = Professor.objects.get(ProfID=id)
     context = {
--- Student/views.py
+++ Student/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from .models import Student
+from django.contrib.auth.decorators import login_required
 # Create your views here.
-
+@login_required
 def student_view(request, id):
     print(type(id))
     obj = Student.objects.get(StudentID=id)
