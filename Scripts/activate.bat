b5efc94bc060347c2cb4052e2654ea0c7297d481
aryan <ar.mikaeili@yahoo.com>
2020-02-04 19:36:52 +0330

    new  Please enter the commit message for your changes. Lines starting

--- Scripts/activate.bat
+++ Scripts/activate.bat
@@ -1,6 +1,6 @@
 @echo off
 
-set "VIRTUAL_ENV=C:\Users\Aryan\Desktop\HW6\FeedbackSystem"
+set "VIRTUAL_ENV=C:\Users\Aryan\Desktop\project_feedback\FeedbackSystem"
 
 if defined _OLD_VIRTUAL_PROMPT (
     set "PROMPT=%_OLD_VIRTUAL_PROMPT%"
